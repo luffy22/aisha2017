b5739b4eb8edf45da278a00feb5f4ac27472bb32
luffy22 <kopnite@gmail.com>
2017-06-27 15:29:06 +0530

    fb comments notification 2017-06-27 @work

--- plugins/content/jw_disqus/jw_disqus/tmpl/article.php
+++ plugins/content/jw_disqus/jw_disqus/tmpl/article.php
@@ -28,6 +28,8 @@ defined('_JEXEC') or die;
 <li class="nav-item active"><a class="nav-link" href="#fb" role="tab" data-toggle="tab">Facebook</a></li>
 <li class="nav-item"><a class="nav-link" href="#disqus" role="tab" data-toggle="tab">Disqus</a></li>
 </ul>
+<div class="mb-1"></div>
+<p>If you wish for a reply then kindly use Disqus. Facebook does not notify about comment posted on website.</p>
 <!-- Tab panes -->
 <div class="tab-content">
 <div class="mt-1"></div>
