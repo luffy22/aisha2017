dfbf73683925ffe3867ef6ce2a9bd47e4e3965c8
luffy22 <kopnite@gmail.com>
2017-08-01 13:48:31 +0530

    images added & sitemap 2017-08-01 @work

--- plugins/content/askexpert/askexpert.php
+++ plugins/content/askexpert/askexpert.php
@@ -1,17 +1,5 @@
 <?php
-/**
- * @package     Joomla.Plugin
- * @subpackage  Content.pagenavigation
- *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
 defined('_JEXEC') or die;
-/**
- * Pagenavigation plugin class.
- *
- * @since  1.5
- */
 class PlgContentAskExpert extends JPlugin
 {
 	function onContentAfterDisplay($context, &$article, &$params, $limitstart)
