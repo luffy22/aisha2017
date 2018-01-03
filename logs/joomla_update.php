8b1a691f7612ba7f1d273b302e4f33612841ba4d
luffy22 <kopnite@gmail.com>
2018-01-03 20:26:01 +0530

    remove outdated files 2018-01-03 @home

---
+++ logs/joomla_update.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-10-07 11:20:43 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-10-07T11:20:43+00:00	INFO ::1	update	Update started by user Rohan Desai (750). Old version is 3.6.5.
+2017-10-07T11:20:49+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.1-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20171007%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20171007T112116Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=c93b3c32468889ecb07133ea6e0643cd3841adedc6035a9d7aafc2a001d2f59c.
+2017-10-07T11:21:19+00:00	INFO ::1	update	File Joomla_3.8.1-Stable-Update_Package.zip successfully downloaded.
